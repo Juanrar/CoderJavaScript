1296cd5561c22f0df2c67a5f78df2e07ad7fa222
juanrar <juanchilorenzo@gmail.com>
2026-07-21 20:58:36 -0300

    Add data attributes to category buttons and update product loading logic for improved filtering

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 const contenerdorProductos = document.querySelector(".grilla-juegos");
-const botonesCategorias = document.querySelector(".boton-categoria");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector(".titulo-principal")
 let productos = [];
 
 async function traerDatos(){
     try{
         const respuesta = await fetch("./data/juegos.json");
         const datos = await respuesta.json();
-        console.log(datos)
         productos =  datos;
-
     }catch(error){
         console.log("Se produjo un error", error);
         return [];
     }
 }
 
-function cargarProductos() {
-    productos.forEach(producto =>{
+function cargarProductos(productosCategoria) {
+    contenerdorProductos.innerHTML = "";
+    productosCategoria.forEach(producto =>{
+        // Vaciar el contenido
+
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML= `
@@ -33,7 +35,16 @@ function cargarProductos() {
 
 async function inicio(){
     await traerDatos();
-    cargarProductos();
+    cargarProductos(productos);
 }
+
+botonesCategorias.forEach(boton =>{
+    boton.addEventListener("click",(e) =>{
+        let categoria = e.target.dataset.categoria;
+        tituloPrincipal.innerText = categoria != "Todos" ? categoria : "Todos los juegos";
+        let productosCategoria = productos.filter(producto => producto.categorias === categoria || categoria === 'Todos');
+        cargarProductos(productosCategoria);
+    })
+})
 
 inicio();
